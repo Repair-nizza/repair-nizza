ae7f506c25f310d4ef0b3a4f9ed879ab6265497e
Shade <kulakaa073@gmail.com>
2025-12-17 16:32:29 +0200

    fix: changing ow images are fetched

--- src/app/[locale]/(otherpages)/portfolio/page.js
+++ src/app/[locale]/(otherpages)/portfolio/page.js
@@ -5,61 +5,67 @@ import BottomCTA from "@/components/shared/bottomCTA/BottomCTA";
 import { client } from "@/sanityClient";
 
 async function getProjects() {
-  const query = `*[_type == "project"] {
+    const query = `*[_type == "project"] {
     _id,
     title,
     subtitle,
     mainImage {
       asset->
     },
+    mobileMainImage {
+      asset->
+    },
     slug,
     category
   }`;
 
-  return client.fetch(query);
+    return client.fetch(query);
 }
 
 async function getCategories() {
-  const query = `*[_type == "category"] {
+    const query = `*[_type == "category"] {
     _id,
     name,
     title
   }`;
 
-  return client.fetch(query);
+    return client.fetch(query);
 }
 
 export async function generateMetadata({ params }) {
-  const { locale } = params;
+    const { locale } = params;
 
-  return {
-    title: {
-      ru: "Наше портфолио | Solide Renovation",
-      en: "Our Portfolio | Solide Renovation",
-      fr: "Notre Portfolio | Solide Renovation",
-    }[locale],
-    description: {
-      ru: "Мы гордимся каждым проектом, от косметического ремонта до ремонта под ключ. Ознакомьтесь с нашими работами и вдохновитесь идеями для вашего дома.",
-      en: "We take pride in every project we undertake, from cosmetic repairs to full turnkey renovation. Explore our work and get inspired for your home.",
-      fr: "Nous sommes fiers de chaque projet, de la rénovation cosmétique à la rénovation complète. Découvrez nos réalisations et inspirez-vous pour votre maison.",
-    }[locale],
-  };
+    return {
+        title: {
+            ru: "Наше портфолио | Solide Renovation",
+            en: "Our Portfolio | Solide Renovation",
+            fr: "Notre Portfolio | Solide Renovation",
+        }[locale],
+        description: {
+            ru: "Мы гордимся каждым проектом, от косметического ремонта до ремонта под ключ. Ознакомьтесь с нашими работами и вдохновитесь идеями для вашего дома.",
+            en: "We take pride in every project we undertake, from cosmetic repairs to full turnkey renovation. Explore our work and get inspired for your home.",
+            fr: "Nous sommes fiers de chaque projet, de la rénovation cosmétique à la rénovation complète. Découvrez nos réalisations et inspirez-vous pour votre maison.",
+        }[locale],
+    };
 }
 
 const Page = async () => {
-  const projects = await getProjects();
-  const categories = await getCategories();
+    const projects = await getProjects();
+    const categories = await getCategories();
 
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow overflow-x-hidden">
-        <PortfolioComponent projects={projects} categories={categories} />
-      </main>
-      <BottomCTA />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow overflow-x-hidden">
+                <PortfolioComponent
+                    projects={projects}
+                    categories={categories}
+                />
+            </main>
+            <BottomCTA />
+            <Footer />
+        </div>
+    );
 };
 
 export default Page;
