7a1d86177a775d3237e18ad41b8f157ad3170423
Shade <kulakaa073@gmail.com>
2025-12-19 19:53:58 +0200

    feat: styles

--- src/components/homepage/WeWorkWith.jsx
+++ src/components/homepage/WeWorkWith.jsx
@@ -3,16 +3,55 @@
 import MarqueeLine from "../shared/marquee/MarqueeLine";
 import Container from "../Container";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+import linesMob from "../../../public/images/image/marquee/linesMob.webp";
+import linesDesk from "../../../public/images/image/marquee/linesDesk.webp";
+import leavesMob from "../../../public/images/image/marquee/leavesMob.webp";
+import leavesDesk from "../../../public/images/image/marquee/leavesDesk.webp";
 
 export default function WeWorkWith() {
     const t = useTranslations("weWorkWith");
 
     return (
-        <section>
-            <Container>
-                <h2>{t("title")}</h2>
-                <MarqueeLine />
+        <section className="relative pb-[104px] lg:pb-[56px]">
+            <div className="absolute top-[-58px] left-1/2 -translate-x-1/2 -z-20 lg:hidden w-[640px] h-[412px]">
+                <Image
+                    src={linesMob}
+                    alt="linesMob"
+                    className="w-full h-full object-cover"
+                    loading="lazy"
+                />
+            </div>
+            <div className="absolute top-[-184px] left-[50%] -z-20 lg:block hidden w-[1016px] h-[625px]">
+                <Image
+                    src={linesDesk}
+                    alt="linesDesk"
+                    className="w-full h-full object-cover"
+                    loading="lazy"
+                />
+            </div>
+            <Image
+                src={leavesMob}
+                alt="leavesMob"
+                width={210}
+                height={210}
+                className="absolute bottom-[-50px] left-[50%] -z-20 lg:hidden"
+                loading="lazy"
+            />
+            <Image
+                src={leavesDesk}
+                alt="leavesDesk"
+                width={316}
+                height={316}
+                className="absolute top-[-102px] left-[31%] -z-20 lg:block hidden"
+                loading="lazy"
+            />
+            <Container className="pt-[114px] lg:pt-[124px] pb-[44px] lg:pb-[41px]">
+                <h2 className="font-arsenal text-[32px] lg:text-[64px] text-primary-black uppercase leading-[125%]">
+                    {t("title")}
+                </h2>
             </Container>
+            <MarqueeLine />
         </section>
     );
 }
--- src/components/shared/marquee/MarqueeLine.jsx
+++ src/components/shared/marquee/MarqueeLine.jsx
@@ -1,44 +1,101 @@
 "use client";
 import Marquee from "react-fast-marquee";
-import * as motion from "motion/react-client";
-import { headerVariants } from "@/utils/animationVariants";
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const marqueeImages = [
+    "41zero42.svg",
+    "adl.svg",
+    "agape.svg",
+    "agapecasa.svg",
+    "arco.svg",
+    "astep.svg",
+    "baxter.svg",
+    "bisazza.svg",
+    "bnbatialia.svg",
+    "boffi.svg",
+    "brokis.svg",
+    "carpetedition.svg",
+    "casabath.svg",
+    "cassina.svg",
+    "cea.svg",
+    "dada.svg",
+    "davidegroppi.svg",
+    "dcweditions.svg",
+    "dk3.svg",
+    "ethimo.svg",
+    "fantini.svg",
+    "flexform.svg",
+    "flos.svg",
+    "gervasoni.svg",
+    "gessi.svg",
+    "giopatocoombes.svg",
+    "gloster.svg",
+    "gypsum.svg",
+    "hotbath.svg",
+    "inhosomething.svg",
+    "itlas.svg",
+    "ivanoredaelli.svg",
+    "karakter.svg",
+    "laborlegno.svg",
+    "listonegiordano.svg",
+    "living.svg",
+    "lumina.svg",
+    "marchicucine.svg",
+    "matteobrioni.svg",
+    "maxalto.svg",
+    "meridiani.svg",
+    "minotti.svg",
+    "molteninc.svg",
+    "nanimarquina.svg",
+    "nemo.svg",
+    "poliform.svg",
+    "polrona.svg",
+    "rimadesio.svg",
+    "salvatori.svg",
+    "santancole.svg",
+    "timenstyle.svg",
+    "viero.svg",
+    "vispring.svg",
+    "zampieri.svg",
+    "zanotta.svg",
+];
 
 export default function MarqueeLine() {
     return (
         <motion.div
-            initial="hidden"
-            whileInView="visible"
-            exit="exit"
+            initial={{ opacity: 0.01 }}
+            whileInView={{
+                opacity: 1,
+                transition: { duration: 0.5, ease: [0.25, 0.1, 0.25, 1] },
+            }}
+            exit={{
+                opacity: 0,
+                transition: { duration: 0.3, ease: [0.42, 0, 1, 1] },
+            }}
             viewport={{ once: true, amount: 0.2 }}
-            variants={headerVariants}
+            className="w-full"
         >
             <Marquee
                 autoFill={true}
                 speed={70}
-                className={twMerge(
-                    clsx(
-                        `h-[42px] lg:h-[76px] border-y lg:border-y-2 text-[17px] lg:text-[30px] leading-none ${
-                            variant === "green"
-                                ? "text-green border-green"
-                                : variant === "light-green"
-                                ? "text-green-light-2 border-green-light-2"
-                                : "text-green border-green"
-                        }`,
-                        className
-                    )
-                )}
+                className="h-[76px] w-full bg-primary-black flex items-center"
             >
-                <span className="inline-block mx-[7.15px] lg:mx-[13px]">
-                    efedra
-                </span>
-                <span className="inline-block mx-[7.15px] lg:mx-[13px]">
-                    efedra
-                </span>
-                <span className="inline-block mx-[7.15px] lg:mx-[13px]">
-                    efedra
-                </span>
+                {marqueeImages.map((image, index) => (
+                    <div
+                        key={index}
+                        className="inline-flex items-center justify-center mx-5 h-[62px]"
+                    >
+                        <Image
+                            src={`/images/SVG/marquee/${image}`}
+                            alt={image.replace(".svg", "")}
+                            width={0}
+                            height={62}
+                            className="h-[62px] w-auto object-contain"
+                            unoptimized
+                        />
+                    </div>
+                ))}
             </Marquee>
         </motion.div>
     );
