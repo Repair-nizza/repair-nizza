dde449e9db356e568bc54050dce7f51788abf02c
Shade <kulakaa073@gmail.com>
2025-12-22 18:49:00 +0200

    fix: adding facebook link

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -6,6 +6,6 @@ export const TELEGRAM_USERNAME = "SolideRenovation";
 
 export const WHATSAPP_USERNAME = "+33778270937";
 
-export const FACEBOOK_USERNAME = "solide.renovation";
+export const FACEBOOK_PROFILE_ID = "61585466821919";
 
 export const CODE_SITE_URL = "https://www.code-site.art/";
