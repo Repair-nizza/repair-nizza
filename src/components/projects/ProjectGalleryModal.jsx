7de8a2ba7d3cc52343bf8a6c1249e38fb12aab9d
Shade <kulakaa073@gmail.com>
2025-12-27 22:07:14 +0200

    fix: updating control buttons

--- src/components/projects/ProjectGalleryModal.jsx
+++ src/components/projects/ProjectGalleryModal.jsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { SlideshowLightbox } from "lightbox.js-react";
+import Image from "next/image";
+import galleryIconBtnDesk from "../../../public/images/SVG/gallery-icon-btn-desk.svg";
+import galleryIconBtnMob from "../../../public/images/SVG/gallery-icon-btn-mob.svg";
+import closeIcon from "../../../public/images/SVG/close-icon.svg";
 
 export default function ProjectGalleryModal({
   gallery,
@@ -32,6 +36,44 @@ export default function ProjectGalleryModal({
     }
   };
 
+  const CustomPrevArrow = () => (
+    <Image
+      src={galleryIconBtnDesk}
+      alt="Previous"
+      width={60}
+      height={60}
+      className="rotate-180 md:w-[60px] md:h-[60px] lg:w-[74px] lg:h-[74px] border border-primary-white rounded-full"
+      style={{ display: 'block', width: '60px', height: '60px', minWidth: '60px', minHeight: '60px' }}
+    />
+  );
+
+  const CustomNextArrow = () => (
+    <Image
+      src={galleryIconBtnDesk}
+      alt="Next"
+      width={60}
+      height={60}
+      className="md:w-[60px] md:h-[60px] lg:w-[74px] lg:h-[74px] border border-primary-white rounded-full"
+      style={{ display: 'block', width: '60px', height: '60px', minWidth: '60px', minHeight: '60px' }}
+    />
+  );
+
+  const CustomCloseButton = () => (
+    <Image
+      src={closeIcon}
+      alt="Close"
+      width={14}
+      height={14}
+      className="shrink-0 absolute top-5 right-5 md:top-4 md:right-4 w-3.5 h-3.5 md:w-4 md:h-4 lg:w-5 lg:h-5 z-[9999]"
+      style={{ 
+        filter: 'brightness(0) invert(1)',
+        display: 'block',
+        minWidth: '14px',
+        minHeight: '14px'
+      }}
+    />
+  );
+
   return (
     <SlideshowLightbox
       images={lightboxImages}
@@ -41,8 +83,11 @@ export default function ProjectGalleryModal({
       onClose={handleClose}
       onSelect={handleSelect}
       showThumbnails={false}
-      showControls={true}
+      showControls={false}
       showArrows={true}
+      prevArrow={<CustomPrevArrow />}
+      nextArrow={<CustomNextArrow />}
+      closeComponent={<CustomCloseButton />}
       className="lightbox-custom"
     />
   );
